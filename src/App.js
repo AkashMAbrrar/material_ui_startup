19b02cee91522205b30e94e8da9ec1b2740b3a63
AkashAbrrar <mabrrar9@gmail.com>
2022-07-06 22:17:09 +0600

    this is first commit for added all the basics componenst and files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from '../src/components/Header/Header';
+import Banner from '../src/components/Banner/Banner';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Banner></Banner>
+      <h1 style={{ paddingTop: '45px', textAlign: 'center' }}>Hello From Another Dimension</h1>
+    </div>
+  );
+}
+
+export default App;
