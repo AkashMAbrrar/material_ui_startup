a0ef5f2e2a374266b135405622928bf086d708fa
AkashAbrrar <mabrrar9@gmail.com>
2022-07-06 23:37:01 +0600

    this commit for cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from '../src/components/Header/Header';
 import Banner from '../src/components/Banner/Banner';
+import GridLayout from './components/GridLayout/GridLayout';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header></Header>
       <Banner></Banner>
       <h1 style={{ paddingTop: '45px', textAlign: 'center' }}>Hello From Another Dimension</h1>
+      <GridLayout></GridLayout>
     </div>
   );
 }
